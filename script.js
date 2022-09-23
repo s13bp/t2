984bcc579c412d44722daac350f0696282517b06
s13bp <86206902+s13bp@users.noreply.github.com>
2022-09-23 03:16:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:3000')
+const socket = io('https://s13bp.github.io/t2/')
 const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
